6ee78ffe2b85a8f12f5260d6c61121ea86b6f66e
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-13 22:45:02 +0530

    WeatherAPP slight changes
    WeatherAPP slight changes

--- WeatherApp/src/components/search/index.jsx
+++ WeatherApp/src/components/search/index.jsx
@@ -2,9 +2,19 @@
 
 
 
-export default function Search(){
-
-  return <div>
-    Search
-  </div>
+export default function Search({ search, setSearch, handleSearch }) {
+  return (
+    <div className="search-engine">
+      <input
+        type="text"
+        placeholder="Enter City Name"
+        name="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button onClick={handleSearch}>
+        Search Weather
+      </button>
+    </div>
+  );
 }
--- WeatherApp/src/components/weather/index.jsx
+++ WeatherApp/src/components/weather/index.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import Search from "../search";
+
 export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchWeatherData(param) {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&{appid=d3cbbe24790a6855c4c3d9f1d8a094c0}`
+      );
+
+      const data = await response.json();
+
+      console.log(data, "data");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function handleSearch(search) {
+    fetchWeatherData(search);
+  }
+
   return (
     <div>
-      <Search />
-      Weather
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      <div className="weather-name">Weather</div>
     </div>
   );
 }
